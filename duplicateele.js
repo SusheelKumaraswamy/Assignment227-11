649d9d99b5e75da1daebd231e94f885c62016a6f
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 19:01:19 +0530

    Update duplicateele.js

--- duplicateele.js
+++ duplicateele.js
@@ -1,4 +1,4 @@
-//7)Remove duplicates from an array
+//7)Remove duplicates from an array IIFE
 
 
 var arr1=[1,1,1,2,3,4,5,5,5,6,6];
@@ -24,3 +24,52 @@ function medians(arr1){
 
 console.log(medians(arr1));
 
+//Anonymous Function
+var arr1=[1,1,1,2,3,4,5,5,5,6,6];
+
+
+var duplicate =function (arr1){
+    var temp=0;
+    
+    
+    var arr3=arr1;
+    for(var i=0;i<arr3.length;i++){
+        var count=0;
+        for(var j=i+1;j<arr3.length;j++){
+            if(arr3[i]==arr3[j]){
+                count+=1
+            }
+        }
+        arr3.splice(i,count);
+    }
+    
+    return arr3;
+}
+
+console.log(duplicate(arr1));
+
+
+//Arrow Function
+
+var arr1=[1,1,1,2,3,4,5,5,5,6,6];
+
+
+var duplicate =(arr1)=>{
+    var temp=0;
+    
+    
+    var arr3=arr1;
+    for(var i=0;i<arr3.length;i++){
+        var count=0;
+        for(var j=i+1;j<arr3.length;j++){
+            if(arr3[i]==arr3[j]){
+                count+=1
+            }
+        }
+        arr3.splice(i,count);
+    }
+    
+    return arr3;
+}
+
+console.log(duplicate(arr1));
