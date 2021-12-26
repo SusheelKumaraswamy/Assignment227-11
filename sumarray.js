1ebf5ab9652b7e4c678b906d719aef186baa19ec
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 18:46:17 +0530

    Update sumarray.js

--- sumarray.js
+++ sumarray.js
@@ -1,4 +1,4 @@
-//Sum of all numbers in an array
+//Sum of all numbers in an array IIFE
 arr3=[];
 function sum(arr){
     let sum=0;
@@ -10,3 +10,30 @@ function sum(arr){
     }
 
 console.log(sum([1,2,3,4,5,6,7]));
+
+//sum array anonymous function.
+var sumarr=function(arr){
+    let sum=0;
+    for(var i=0;i<arr.length;i++){
+        sum+=+arr[i];
+            
+        }
+       return sum;
+    }
+
+console.log(sumarr([1,2,3,4,5]));
+
+//sum array arrow function
+var sumarr=(arr)=>{
+    let sum=0;
+    for(var i=0;i<arr.length;i++){
+        sum+=+arr[i];
+            
+        }
+       return sum;
+    }
+
+console.log(sumarr([1,2,3,4,5]));
+
+
+    
