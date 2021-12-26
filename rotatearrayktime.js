b3e96c3457e231f8b03e2de02e1ab504dc7ef694
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 18:57:28 +0530

    Update rotatearrayktime.js

--- rotatearrayktime.js
+++ rotatearrayktime.js
@@ -1,4 +1,4 @@
-//rotate array k time 
+//rotate array k time IIFE
 
 var arr=[1,2,3,4,5,12,12,1,9];
 
@@ -22,3 +22,55 @@ return arr;
 }
 
 console.log(rev(arr,n));
+
+//Anonymous Function
+//rotate array k time 
+
+var arr=[1,2,3,4,5,12,12,1,9];
+
+var n=3;
+
+
+var rotate=function(arr,n){
+    var temp=0;
+    var loop=Math.floor(arr.length/2);
+    var j=0;
+    while(j!=n)
+    {
+        for(var i=0;i<loop;i++){
+            temp=+arr[i];
+            arr[i]=+arr[arr.length-1-i];
+            arr[arr.length-1-i]=+temp;
+            }
+            j++;
+    }
+return arr;
+}
+
+console.log(rotate(arr,n));
+
+//Arrow Function
+//rotate array k time 
+
+var arr=[1,2,3,4,5,12,12,1,9];
+
+var n=3;
+
+
+var rotate=(arr,n)=>{
+    var temp=0;
+    var loop=Math.floor(arr.length/2);
+    var j=0;
+    while(j!=n)
+    {
+        for(var i=0;i<loop;i++){
+            temp=+arr[i];
+            arr[i]=+arr[arr.length-1-i];
+            arr[arr.length-1-i]=+temp;
+            }
+            j++;
+    }
+return arr;
+}
+
+console.log(rotate(arr,n));
