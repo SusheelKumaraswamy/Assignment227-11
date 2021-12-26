e29ea5c16f347970a1b0beeeade0cfb96135b2b8
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 18:50:59 +0530

    Update primenumberarr.js

--- primenumberarr.js
+++ primenumberarr.js
@@ -1,4 +1,4 @@
-//primenumber
+//primenumber IIFE
 
 var num=[2,3,13,12,14,17];
 
@@ -41,3 +41,89 @@ return primenum;
 }
 
 console.log(prime(num));
+
+
+
+
+//anonymous function
+
+//primenumber
+
+var primnumber=function(num){
+
+    var primenum=[];
+    var prime=true;
+    for(var j=0;j<num.length;j++){
+        if(num[j]==1){
+        console.log("neither prime nor composite")
+        }
+        else if(num[j]==2){
+                primenum.push(num[j]);
+            }
+        else
+        {
+            for(var i=2;i<num[j];i++){
+                if(num[j]%i==0){
+                    prime=true;
+                    break;
+                    }
+                else{
+                    //console.log("prime");
+                    prime=false;
+                }
+            }
+        }
+    if(prime==false){
+        primenum.push(num[j]);
+    }
+    
+    
+    
+    }
+    return primenum;
+}
+var num=[2,3,13,12,14,17];
+
+console.log(primnumber(num));
+
+
+//arrow function
+
+//primenumber
+
+var primnumber=(num)=>{
+
+    var primenum=[];
+    var prime=true;
+    for(var j=0;j<num.length;j++){
+        if(num[j]==1){
+        console.log("neither prime nor composite")
+        }
+        else if(num[j]==2){
+                primenum.push(num[j]);
+            }
+        else
+        {
+            for(var i=2;i<num[j];i++){
+                if(num[j]%i==0){
+                    prime=true;
+                    break;
+                    }
+                else{
+                    //console.log("prime");
+                    prime=false;
+                }
+            }
+        }
+    if(prime==false){
+        primenum.push(num[j]);
+    }
+    
+    
+    
+    }
+    return primenum;
+}
+var num=[2,3,13,12,14,17];
+
+console.log(primnumber(num));
