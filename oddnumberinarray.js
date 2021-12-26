71a24749b75f5e800355dbc79305b412fa96dca6
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 18:37:11 +0530

    Update oddnumberinarray.js

--- oddnumberinarray.js
+++ oddnumberinarray.js
@@ -1,4 +1,4 @@
-//Print odd numbers in an array
+//Print odd numbers in an array Immediately Invoked Function Expression - IIFE
 arr3=[];
 function oddnumber(arr){
     for(var i=0;i<arr.length;i++){
@@ -13,3 +13,41 @@ function oddnumber(arr){
 
 oddnumber([0,1,2,3,4]);
 var arr3;
+
+//Print odd numbers in an array Anonymus function.
+
+var odd=function(arr){
+    var arr3=[];
+     for(var i=0;i<arr.length;i++){
+        if(arr[i]!=0 && arr[i]%2!=0){
+            arr3.push(arr[i]);
+            
+        }
+    }
+    return arr3;
+}
+
+console.log(odd([0,1,2,3,4]));
+
+//Print odd numbers in an array arrow function
+
+var arr1=[1,2,3,4,5];
+
+var odd= (arr)=>{
+    var arr3=[];
+     for(var i=0;i<arr.length;i++){
+        if(arr[i]!=0 && arr[i]%2!=0){
+            arr3.push(arr[i]);
+            
+        }
+    }
+    return arr3;
+}
+
+
+console.log(odd(arr1))
+
+
+
+
+
