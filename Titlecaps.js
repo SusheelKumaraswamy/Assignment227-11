da7ea7ffe46472ee2d723fec4db4c0f0a68b08db
SusheelKumaraswamy <93818386+SusheelKumaraswamy@users.noreply.github.com>
2021-12-26 18:42:18 +0530

    Update Titlecaps.js

--- Titlecaps.js
+++ Titlecaps.js
@@ -1,4 +1,4 @@
-//title caps
+//title caps //IIFE
 
 arr3=[];
 function titleCase(arr){
@@ -11,3 +11,35 @@ function titleCase(arr){
     }
 
 console.log(titleCase(["hello","hai"]).join(" "));
+
+//title caps //anonymous function
+var titlecaps=function(arr){
+    var arr3=[];
+    for(var i=0;i<arr.length;i++){
+        arr3.push((arr[i].charAt(0).toUpperCase()+arr[i].
+        substr(1).toLowerCase()));
+            
+        }
+       return arr3;
+    }
+
+console.log(titlecaps(["hello","hai"]));
+
+
+//title caps//arrow
+
+var titlecaps=(arr)=>{
+    var arr3=[];
+    for(var i=0;i<arr.length;i++){
+        arr3.push((arr[i].charAt(0).toUpperCase()+arr[i].
+        substr(1).toLowerCase()));
+            
+        }
+       return arr3;
+    }
+
+console.log(titlecaps(["hello","hai"]));
+
+    
+
+
